27190e257b43e042441561d5b8ff3256eeb5ea16
玖叁 <colour_93@furry.top>
2023-10-28 12:28:14 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@
 
 import { useEffect, useState } from "react";
 import { DataConnection, Peer } from "peerjs";
-import { Button, Input, Space } from "antd";
+import { Button, Col, Input, Row, Space, notification } from "antd";
 
 let peer: Peer | null = null;
 
 export default function Home() {
   const [id, setId] = useState("");
   const [targetId, setTargetId] = useState("");
-  const [deviceList, setDeviceList] = useState<DataConnection[]>([]);
+  const [message, setMessage] = useState("");
+  const [connectionList, setConnectionList] = useState<DataConnection[]>([]);
 
   const [initLoading, setInitLoading] = useState(false);
   const [connecting, setConnecting] = useState(false);
@@ -30,6 +31,34 @@ export default function Home() {
 
     peer.on("connection", (connection) => {
       console.log(connection);
+
+      connection.on("data", (data) => {
+        console.log(data);
+      });
+
+      connection.on("close", () => {
+        setConnectionList(
+          connectionList.filter((conn) => conn.peer == connection.peer)
+        );
+        notification.info({
+          message: "Device Disconnected",
+          description: `ID: ${connection.peer}`,
+        });
+      });
+
+      connection.on("error", (error) => {
+        console.error(error);
+        notification.error({
+          message: "Connection Error",
+          description: error.message,
+        });
+      });
+
+      setConnectionList([connection, ...connectionList]);
+      notification.info({
+        message: `Device Connected`,
+        description: `ID: ${connection.peer}`,
+      });
     });
   };
 
@@ -42,52 +71,79 @@ export default function Home() {
     console.log(targetId);
     const connection = peer.connect(targetId);
     connection.on("open", () => {
+      setConnecting(false);
+      notification.success({
+        message: `Device Connected`,
+        description: `ID: ${targetId}`,
+      });
       console.log(`target ${targetId} connection opend`);
     });
-    setDeviceList([connection, ...deviceList]);
+    setConnectionList([connection, ...connectionList]);
+  };
+
+  const sendMessage = () => {
+    const target = connectionList[0];
+
+    target.send({
+      type: "message",
+      data: {
+        message,
+      },
+    });
   };
 
   return (
-    <div className="p-10">
-      <Space direction="vertical">
-        <Space.Compact>
-          <Input addonBefore="Device ID" value={id} style={{ width: 480 }} />
-          <Button type="primary" onClick={peerInit} loading={initLoading}>
-            Get
-          </Button>
-        </Space.Compact>
+    <Row>
+      <Col span={12}>
+        <Space direction="vertical" className="p-10">
+          <Space.Compact>
+            <Input addonBefore="Device ID" value={id} style={{ width: 480 }} />
+            <Button type="primary" onClick={peerInit} loading={initLoading}>
+              Get
+            </Button>
+          </Space.Compact>
 
-        <Space.Compact>
-          <Input
-            addonBefore="Target ID"
-            value={targetId}
+          <Space.Compact>
+            <Input
+              addonBefore="Target ID"
+              value={targetId}
+              onChange={(e) => {
+                setTargetId(e.target.value);
+              }}
+              style={{ width: 480 }}
+            />
+            <Button
+              type="primary"
+              onClick={connectDevice}
+              loading={connecting}
+              disabled={!peer}
+            >
+              Connect
+            </Button>
+          </Space.Compact>
+
+          <Space direction="vertical">
+            {connectionList.map(({ peer }, index) => (
+              <div key={index}>{peer}</div>
+            ))}
+          </Space>
+        </Space>
+      </Col>
+
+      <Col span={12}>
+        <Space direction="vertical" className="p-10">
+          <Input.TextArea
+            placeholder="Type something here..."
+            value={message}
             onChange={(e) => {
-              setTargetId(e.target.value);
+              setMessage(e.target.value);
             }}
-            style={{ width: 480 }}
           />
-          <Button
-            type="primary"
-            onClick={connectDevice}
-            loading={connecting}
-            disabled={!peer}
-          >
-            Connect
-          </Button>
-        </Space.Compact>
-
-        <Space.Compact>
-          <Input addonBefore="Message" style={{ width: 480 }} />
-          <Button type="primary" onClick={peerInit}>
+          <Button type="primary" onClick={sendMessage}>
             Send
           </Button>
-        </Space.Compact>
-      </Space>
-      {/* <Space direction="vertical">
-        {deviceList.map((deviceId, index) => (
-          <>{deviceId}</>
-        ))}
-      </Space> */}
-    </div>
+        </Space>
+      </Col>
+    </Row>
   );
 }
